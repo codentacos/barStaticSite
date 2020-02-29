6f81a2c0ee49911a156e4c63b6eb23033cd5da13
Zachary Nelson <nelsonz2013@hotmail.com>
2020-02-29 11:01:09 -0600

    updates

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,4 +1,12 @@
 $(document).ready(function() {
-    $('.load-animation').fadeIn(1500);
+    $(".load-animation").fadeIn(1500);
+    $(".load-animation").css('display', 'inline-block');
+
+    setInterval(function() {
+        $(".load-animation-arrow").fadeToggle();
+    }, 2000);
 });
 
+$('.navbar-toggle').on('click', function() {
+    $('.icon-bar').toggleClass('.icon-bar-color-toggle');
+})
